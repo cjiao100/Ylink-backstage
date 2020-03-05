f9c6daf3cd1977404b0bb2e0022ecaeaa85d7881
cjiao100 <1971416012@qq.com>
2020-03-05 15:41:54 +0800

    引入elementUI

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,12 @@
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  display: flex;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 
 import "@/styles/index.scss"; // global css
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
 
